f0e896ceba0090b4ae36f6391d4a05b6ae1c8a54
Nicolas F <fournout.nicolas@gmail.com>
2019-11-04 18:12:17 +0100

    Done

--- game.py
+++ game.py
@@ -1,21 +1,33 @@
 import turtle
 
 def dessine_plateau(n):
+    turtle.up()
+    turtle.goto(-300,-200)
+    turtle.down()
+    i = 0
     if n == 1:
         longueur = 20
     else:
-        longueur = 20 + 30*n
+        longueur = (40 + 30*n)
 
-    turtle.forward(longueur)
+    turtle.forward(longueur*n + 6*n + 20*n+1)
     turtle.right(90)
     turtle.forward(20)
     turtle.right(90)
-    turtle.forward(longueur)
+    turtle.forward(longueur*n + 6*n + 20*n+1)
     turtle.right(90)
     turtle.forward(20)
+    turtle.right(90)
+    while (i != n):
+        turtle.forward(20 + longueur/2)
+        turtle.left(90)
+        turtle.forward(longueur/2)
+        turtle.right(90)
+        turtle.forward(6)
+        turtle.right(90)
+        turtle.forward(longueur/2)
+        turtle.left(90)
+        turtle.forward(longueur/2)
+        i += 1
 
-    turtle.forward(longueur * 1/3)
-    turtle.left(90)
-    turtle.forward(longueur * 1/3)
-    
 dessine_plateau(3)
