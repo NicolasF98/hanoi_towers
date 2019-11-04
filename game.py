fc3488e2c2a0a10bd0fa748441948a56578cf43a
Nicolas F <fournout.nicolas@gmail.com>
2019-11-04 18:43:11 +0100

    Pb alignement

--- game.py
+++ game.py
@@ -4,20 +4,20 @@ def dessine_plateau(n):
     turtle.up()
     turtle.goto(-300,-200)
     turtle.down()
+
     i = 0
     if n == 1:
         longueur = 20
     else:
         longueur = (40 + 30*n)
+    while (i != 2):
+        turtle.forward(longueur*n + 6*n + 20*n+1)
+        turtle.right(90)
+        turtle.forward(20)
+        turtle.right(90)
+        i += 1
+    i = 0
 
-    turtle.forward(longueur*n + 6*n + 20*n+1)
-    turtle.right(90)
-    turtle.forward(20)
-    turtle.right(90)
-    turtle.forward(longueur*n + 6*n + 20*n+1)
-    turtle.right(90)
-    turtle.forward(20)
-    turtle.right(90)
     while (i != n):
         turtle.forward(20 + longueur/2)
         turtle.left(90)
@@ -31,3 +31,26 @@ def dessine_plateau(n):
         i += 1
 
 dessine_plateau(3)
+
+def dessine_disque(nd, plateau, n):
+    turtle.up()
+    turtle.goto(-300,-200)
+    turtle.forward(20 + 15*(n-nd))
+    turtle.left(90)
+    turtle.forward(20 + 20*(n-nd))
+    turtle.right(90)
+
+    turtle.down()
+
+    turtle.forward(40 + 30*nd)
+    turtle.right(90)
+    turtle.forward(20)
+    turtle.right(90)
+    turtle.forward(40 + 30*nd)
+    turtle.right(90)
+    turtle.forward(20)
+    turtle.right(90)
+
+dessine_disque(3,0,3)
+dessine_disque(2,0,3)
+dessine_disque(1,0,3)
