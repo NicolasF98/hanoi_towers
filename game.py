a0a4bc5438c1ae626d2c86d3b8ea3118986ccb79
Nicolas F <fournout.nicolas@gmail.com>
2019-11-04 18:59:35 +0100

    pb alignement

--- game.py
+++ game.py
@@ -11,7 +11,7 @@ def dessine_plateau(n):
     else:
         longueur = (40 + 30*n)
     while (i != 2):
-        turtle.forward(longueur*n + 6*n + 20*n+1)
+        turtle.forward(longueur*n + 20*n+1)
         turtle.right(90)
         turtle.forward(20)
         turtle.right(90)
@@ -19,15 +19,18 @@ def dessine_plateau(n):
     i = 0
 
     while (i != n):
-        turtle.forward(20 + longueur/2)
+        turtle.forward(20 + longueur/2 )
         turtle.left(90)
         turtle.forward(longueur/2)
         turtle.right(90)
         turtle.forward(6)
         turtle.right(90)
         turtle.forward(longueur/2)
-        turtle.left(90)
+        turtle.right(90)
+        turtle.forward(6)
+        turtle.right(180)
         turtle.forward(longueur/2)
+    
         i += 1
 
 dessine_plateau(3)
