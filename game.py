4cb086dcd2c2aa41b227ded14a41a0818e9aee3a
Nicolas F <fournout.nicolas@gmail.com>
2019-11-07 14:03:06 +0100

    refonte de la partie graphique

--- game.py
+++ game.py
@@ -12,7 +12,7 @@ def init(n):
 def nombre_disques(plateau, numtour):
     return len(plateau[numtour])
 
-print(nombre_disques(init(3),0))
+#print(nombre_disques(init(3),0))
 
 
 def disque_superieur(plateau, numtour):
@@ -20,7 +20,7 @@ def disque_superieur(plateau, numtour):
         return -1
     return plateau[numtour][0]
 
-print(disque_superieur(init(2),0))
+#print(disque_superieur(init(2),0))
 
 def position_disque(plateau, numdisque):
     i, j = 0, 0
@@ -32,7 +32,7 @@ def position_disque(plateau, numdisque):
         i += 1
     return -1
 
-print(position_disque(init(5),5))
+#print(position_disque(init(5),5))
 
 def verifier_deplacement(plateau, nt1, nt2):
     if (len(plateau[nt1]) == 0):
@@ -46,7 +46,7 @@ def verifier_deplacement(plateau, nt1, nt2):
             else:
                 return False
 p = [[2],[3],[1]]
-print(verifier_deplacement(p,0,2))
+#print(verifier_deplacement(p,0,2))
 
 
 def verifier_victoire(plateau, n):
@@ -60,7 +60,7 @@ def verifier_victoire(plateau, n):
     return val
 
 p = [[], [], [3,2,1]]
-print(verifier_victoire(p, 3))
+#print(verifier_victoire(p, 3))
 
 
 
@@ -71,79 +71,4 @@ def dessine_plateau(n):
     turtle.goto(-300,-200)
     turtle.down()
 
-    i = 0
-    if n == 1:
-        longueur = 20
-    else:
-        longueur = (40 + 30*n)
-    while (i != 2):
-        turtle.forward(longueur*n + 20*n+1)
-        turtle.right(90)
-        turtle.forward(20)
-        turtle.right(90)
-        i += 1
-    i = 0
-
-    while (i != n):
-        turtle.forward(20 + longueur/2 )
-        turtle.left(90)
-        turtle.forward(longueur/2)
-        turtle.right(90)
-        turtle.forward(6)
-        turtle.right(90)
-        turtle.forward(longueur/2)
-        turtle.right(90)
-        turtle.forward(6)
-        turtle.right(180)
-        turtle.forward(longueur/2)
     
-        i += 1
-
-#dessine_plateau(3)
-
-def dessine_disque(nd, plateau, n):
-    turtle.up()
-    turtle.goto(-300,-200)
-    turtle.forward(20 + 15*(n-nd))
-    turtle.left(90)
-    turtle.forward(20 + 20*(n-nd))
-    turtle.right(90)
-
-    turtle.down()
-
-    turtle.forward(40 + 30*nd)
-    turtle.right(90)
-    turtle.forward(20)
-    turtle.right(90)
-    turtle.forward(40 + 30*nd)
-    turtle.right(90)
-    turtle.forward(20)
-    turtle.right(90)
-
-#dessine_disque(3,0,3)
-#dessine_disque(2,0,3)
-#dessine_disque(1,0,3)
-
-def efface_disque(nd, plateau, n):
-    turtle.color("White")
-    turtle.up()
-    turtle.goto(-300,-200)
-    turtle.forward(20 + 15*(n-nd))
-    turtle.left(90)
-    turtle.forward(20 + 20*(n-nd))
-    turtle.right(90)
-
-    turtle.down()
-
-    turtle.forward(40 + 30*nd)
-    turtle.right(90)
-    turtle.forward(20)
-    turtle.right(90)
-    turtle.forward(40 + 30*nd)
-    turtle.right(90)
-    turtle.forward(20)
-    turtle.right(90)
-
-#efface_disque(3,0,3)
-#efface_disque(2,0,3)
-#efface_disque(1,0,3)
