6f56e494c73e3b7e5fde492d5da9388a6105ff7d
Nicolas F <fournout.nicolas@gmail.com>
2019-11-28 11:54:44 +0100

    .

--- partie_c.py
+++ partie_c.py
@@ -6,12 +6,16 @@ def lire_coords(plateau):
     valide = 0
     coords = []
     while (valide != 1):
-        t_dep = int(input("Choisis tour de départ: "))
+        t_dep = int(input("Choisis la tour de départ: "))
         if (0 <= t_dep <= 2 and ((len(plateau[t_dep]) != 0))):
             valide = 1
+        elif ((len(plateau[t_dep]) == 0)):
+            print("Invalide, tour vide.")
+        elif (0 > t_dep > 2):
+            print("Invalide, la tour n'existe pas.")
     coords.append(t_dep)
     while (valide != 0):
-        t_arriv = int(input("Choisis tour d'arrivée: "))
+        t_arriv = int(input("Choisis la tour d'arrivée: "))
         if (0 <= t_arriv <= 2):
             if (len(plateau[t_arriv]) == 0):
                 valide = 0
@@ -36,9 +40,10 @@ def jouer_un_coup(plateau, n):
 
 def boucle_jeu(plateau, n):
     win = verifier_victoire(plateau,n)
-    cpt = 0 #compteur de coups joué
+    cpt = 1 #compteur de coups joué
     cpt_max = 10 #on initialise le nbr de coup max à 10
-    while ((win != True) and (cpt < cpt_max)):
+    while ((win != True) and (cpt <= cpt_max)):
+        print("Coup numéro ",cpt)
         jouer_un_coup(plateau,n)
         print(plateau)
         win = verifier_victoire(plateau,n)
@@ -46,6 +51,7 @@ def boucle_jeu(plateau, n):
     print("Formidable ! tu as gagné en:",cpt,"coups ! \nTu es vraiment beaucoup trop fort.")
 
 def main():
+    print("-- Bienvenue dans les Tours de Hanoi --")
     n = int(input("Avec combien de disque souhaites-tu jouer ? "))
     liste_plateau = init(n)
     dessine_plateau(n)
