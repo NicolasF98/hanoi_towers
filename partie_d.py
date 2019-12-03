05f9d6cf0d8834c1e3dbd47d2eb8a829f262d865
Nicolas F <fournout.nicolas@gmail.com>
2019-12-03 11:07:48 +0100

    .

--- partie_d.py
+++ partie_d.py
@@ -5,7 +5,7 @@
 #la valeur la config du plateau
 
 def dernier_coup(dict, num_der_tour):
-    return dict[numer_der_tour]
+    return dict[num_der_tour]
 
 def annuler_dernier_coup(dict, num_der_tour):
     return dict[num_der_tour].reverse()
