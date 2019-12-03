82b12e0a8e334404887d996a1ba0aafb372edf0c
Nicolas F <fournout.nicolas@gmail.com>
2019-12-03 11:26:12 +0100

    .

---
+++ partie_e.py
@@ -0,0 +1,21 @@
+import time
+
+def score_joueur(nom, nbr_disque, nbr_coups):
+    #nous allons ajouter le nom ainsi que le
+    #score du joueur dans note fichier 'score.txt'.
+    score = open("score.txt", "a")
+
+    score.write("Nom du joueur: ")
+    score.write(nom.upper())
+    score.write('\n')
+    score.write("Nbr_disque: ")
+    score.write(nbr_disque)
+    score.write('\n')
+    score.write("Nbr_coups: ")
+    score.write(nbr_coups)
+    score.write('\n')
+    score.write('\n')
+
+    #on oublie pas de fermer notre fichier une fois
+    #l'ajout effectué.
+    score.close()
