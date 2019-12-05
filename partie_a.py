19ff19607b2fe843f9a518b76a0867acadff8fc7
Eric fournout <fournout.nicolas@gmail.com>
2019-12-05 12:58:50 +0100

    partie_a finie

--- partie_a.py
+++ partie_a.py
@@ -1,88 +1,94 @@
 #PARTIE A: Plateau de et listes
 
 def init(n):
-    #on créer une variable config_init qui contient une liste des nombre de 1 à n.
+    #On créer une variable config_init qui contient une liste des nombre de 1 à n.
     config_init = list(range(1, n+1))
 
-    #on inverse notre liste car à sa création elle est inversé.
+    #On inverse notre liste car à sa création elle est inversé.
     config_init.reverse()
 
-    #creation d'une liste de 3 liste qui représente notre plateau et ses 3 tours.
+    #Création d'une liste de 3 listes qui représente notre plateau et ses 3 tours.
     plateau = [[],[],[]]
 
-    #on ajoute à l'indice 0 (donc à la tour 1) la liste config_init, notre plateau est donc bien initialisé.
+    #On ajoute à l'indice 0 (donc à la tour 1) la liste config_init, notre plateau est donc bien initialisé.
     plateau[0] = list(config_init)
+
     return plateau
 
+
 def nombre_disques(plateau, numtour):
-    #on revoint le nombre d'élements situé sur la tour donnée en argument.
+    #On revoint le nombre d'élements situé sur la tour donnée en argument.
     return len(plateau[numtour])
 
+
 def disque_superieur(plateau, numtour):
-    #si la tour est vide on renvoi -1.
+    #Si la tour est vide on renvoi -1.
     if (len(plateau[numtour]) == 0):
         return -1
 
-    #sinon on renvoi le plus petit élément placé sur la tour.
+    #Sinon on renvoi le plus petit élément placé sur la tour.
     return plateau[numtour][len(plateau[numtour])-1]
+
 
 def position_disque(plateau, numdisque):
     tour, index_disque = 0, 0
 
-    #on parcours toutes les tours
+    #On parcourt toutes les tours.
     while (tour != 2):
 
-        #on parcours tous les index de disque
+        #On parcourt tous les index de disque.
         while (index_disque != len(plateau)):
 
-            #si on trouve notre element, on renvoie sa position
+            #Si on trouve notre élément, on renvoit sa position.
             if (plateau[tour][index_disque] == numdisque):
                 return tour, index_disque
 
-            #si on ne le trouve pas on incremente notre index
+            #Si on ne le trouve pas on incrémente notre index.
             index_disque += 1
 
-        #après avoir parcouru tous les index d'une tour, si on ne trouve toujours pas notre element,
-        #on reinitialise notre index a 0 et on passe à la tour suivante.
+        #Après avoir parcouru tous les index d'une tour, si on ne trouve toujours pas notre élément,
+        #on reinitialise notre index à 0 et on passe à la tour suivante.
         index_disque = 0 
         tour += 1
 
-    #si on ne trouve toujours pas après avoir parcouru tous les index de toutes les tours,
-    #l'élément n'existe pas et on renvoi alors -1
+    #Si on ne trouve toujours notre élément pas après avoir parcouru tous les index de toutes les tours,
+    # alors l'élément n'existe pas et on renvoi alors -1.
     return -1
 
+
 def verifier_deplacement(plateau, nt1, nt2):
-    #si la tour initial est vide, on return False
+    #Si la tour initial est vide, on return False.
     if (len(plateau[nt1]) == 0):
         return False
 
-    #sinon on regarde si la tour final est cohérente
+    #Sinon on regarde si la tour final est cohérente.
     else:
 
-        #si la tour final est vide, le deplacement est valide, on return True
+        #si la tour final est vide, le déplacement est valide, on return True.
         if (len(plateau[nt2]) == 0):
             return True
 
-        #si elle n'est pas vide, on verifie que le disque supérieur de la tour inital est inférieur à celui de la tour final
-        #si c'est le cas le deplacement est valide (return True), sinon il ne l'est pas (return False)
+        #Si elle n'est pas vide, on vérifie que le disque supérieur de la tour inital est inférieur à celui de la tour final,
+        # si c'est le cas le déplacement est valide (return True), sinon il ne l'est pas (return False).
         elif (len(plateau[nt2]) != 0):
             if (disque_superieur(plateau,nt1) < disque_superieur(plateau,nt2)):
                return True
             else:
                 return False
 
+
 def verifier_victoire(plateau, n):
-    #On initialise 2 listes qui vont correspondre au tableau de victoire pour n éléments.
+    #On initialise une listes qui vas correspondre au tableau de victoire pour n éléments.
     victoire = [[], [], []]
     
-    #on ajoute sur la tour numéro 3 les éléments de 1 à n dans nos deux listes.
+    #on ajoute sur la tour numéro 3 les éléments de 1 à n dans notre liste.
     i = n
     while (i >= 1):
         victoire[2].append(i)
         i -= 1
 
-    #on test si notre plateau correspond à une des liste de victoire,
-    #si c'est le cas on return True, sinon False
+    #On test si notre plateau correspond à la liste de victoire,
+    # si c'est le cas on return True, sinon False.
     if (victoire == plateau):
         return True
     return False
