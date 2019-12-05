abec458bf11795ded17c70ca7953678fda6501de
Eric fournout <fournout.nicolas@gmail.com>
2019-12-05 13:15:21 +0100

    partie_c finie

--- partie_c.py
+++ partie_c.py
@@ -11,10 +11,10 @@ def lire_coords(plateau):
     valide_dep = 0
     valide_arr = 0
 
-    #creation de la liste de sortie de nos coordonnées.
+    #Création de la liste de sortie de nos coordonnées.
     coords = []
 
-    #on boucle temps que le joueur n'a pas donnée une coord de départ valide.
+    #On boucle temps que le joueur n'a pas donnée une coord de départ valide.
     while (valide_dep != 1):
         t_dep = int(input("Choisis la tour de départ: "))
         if (0 <= t_dep <= 2 and ((len(plateau[t_dep]) != 0))):
@@ -25,7 +25,7 @@ def lire_coords(plateau):
             print("Invalide, la tour n'existe pas.")
     coords.append(t_dep)
 
-    #on boucle temps que le joueur n'a pas donnée une coord de départ valide.
+    #On boucle temps que le joueur n'a pas donnée une coord d'arrivé valide.
     while (valide_arr != 1):
         t_arriv = int(input("Choisis la tour d'arrivée: "))
         if (0 <= t_arriv <= 2):
@@ -39,74 +39,74 @@ def lire_coords(plateau):
 
 
 def jouer_un_coup(plateau, n):
-    #on recupére les coordonnées de deplacement fournis par le joueur.
+    #On récupére les coordonnées de déplacement fournis par le joueur.
     coords = lire_coords(plateau)
 
-    #on récupère la valeur de depart demandé par le joueur.
+    #On récupère la valeur de départ demandé par le joueur.
     val = plateau[coords[0]][len(plateau[coords[0]])-1]
 
-    #on supprime la position initiale du disque sur notre plateau.
+    #On supprime la position initiale du disque sur notre plateau.
     efface_disque(val, plateau,n)
 
-    #on déplace notre disque sur sa nouvelle position dans notre liste representant notre plateau.
+    #On déplace notre disque sur sa nouvelle position dans notre liste représentant notre plateau.
     plateau[coords[0]].remove(val)
     plateau[coords[1]].append(val)
 
-    #on dessine la nouvelle position de notre disque.
+    #On dessine la nouvelle position de notre disque.
     dessine_disque(val, plateau, n)
 
     return coords
 
 def boucle_jeu(plateau, n):
-    #la variable win correspond à la condition de victoire, en fonction du plateau et de nbr de disque.
+    #La variable `win` correspond à la condition de victoire, en fonction du plateau et de nbr de disque.
     win = verifier_victoire(plateau,n)
 
-    #creation du dictionnaire qui nous permet de recuperere la paire de mouvement du joueur, en fonction du coup.
-    coups = {}
+    #Création du dictionnaire qui nous permet de récupèrere la paire de mouvement du joueur, en fonction du coup.
+    coups = {}     # PARTIE D #
 
-    #compteur de coups joué.
+    #Compteur de coups joué.
     cpt = 1 
 
-    #variable qui nous sert de booléen d'annulation.
+    #Variable qui nous sert de booléen d'annulation.
     annuler = 0
 
-    #on initialise le nbr de coup max à 10.
+    #On initialise le nbr de coup max à 10.
     cpt_max = 10
 
-    #on boucle temps que le joueur n'a pas gagné ou que le nbr max de coup n'est pas atteind.
+    #On boucle temps que le joueur n'a pas gagné ou que le nbr max de coup n'est pas atteint.
     while (win != True) and (cpt <= cpt_max):
         print("Coup numéro ",cpt)
 
-        #recupère les coordonées donnée par le joueur au coup cpt, cela nous permet de faire un suivi 
-        #des coups qu'on pourra annuler par la suite.
-        coups[cpt] = jouer_un_coup(plateau,n)
+        #On récupère les coordonées fournit par le joueur au coup cpt, cela nous permet de faire un suivi 
+        # des coups qu'on pourra annuler par la suite.
+        coups[cpt] = jouer_un_coup(plateau,n)        # PARTIE D #
 
-        #on propose a l'utilisateur de d'annuler son coup.
+        #On propose à l'utilisateur d'annuler son coup. 1 pour oui, 0 pour non.
         annuler = int(input("Veux-tu annuler ce coup ? (1/0) "))
 
-        #si il accepte, on appelle notre fonction annuler_dernier_coup et on decremente notre compteur.
+        #Si il accepte, on appelle notre fonction annuler_dernier_coup et on décremente notre compteur.
         if (annuler == 1):
             annuler_dernier_coup(coups, cpt, plateau, n)
             cpt += -1
         
-        #on remette notre variable annuler à 0 afin d'éviter des problèmes booleen au prochain tour de boucle.
+        #0n reinitialise notre variable annuler à 0 afin d'éviter des problèmes de booleen au prochain tour de boucle.
         annuler = 0
 
-        #on verifie si on a reussi notre jeu, on stock  le resultat dans une variable `win`.
+        #On vérifie si on à réussi notre jeu, on stock le resultat dans une variable `win`.
         win = verifier_victoire(plateau,n)
         cpt += 1
     
-    #si le joueur à joué trop de coups, il a perdu.
+    #Si le joueur a joué trop de coups, il a perdu.
     if (cpt >= cpt_max):
         print("Désolé tu as perdu !")
     
-    #sinon il a gagné.
+    #Sinon il a gagné.
     else:
         print("Formidable ! tu as gagné en:",cpt,"coups ! \nTu es vraiment beaucoup trop fort.")
         nom_joueur = input("Quel est ton nom jeune joueur ?")
 
-    #on ajoute le score de 'nom_joueur' dans notre txt avec l'aide de la fonction 'score_joueur'.
-    score_joueur(nom_joueur, str(n), str(cpt))
+    #On ajoute le score de 'nom_joueur' dans notre txt avec l'aide de la fonction 'score_joueur'.
+    score_joueur(nom_joueur, str(n), str(cpt))          # PARTIE E #
 
 
 #definition d'une fonction main comme demandé.
